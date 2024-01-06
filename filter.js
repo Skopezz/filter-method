134667848fdd5f4f67602d3841502c783355d2cf
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-06 14:46:12 +0000

    add complex filtering

--- filter.js
+++ filter.js
@@ -61,3 +61,7 @@ const students = [
   },
 ];
 
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
